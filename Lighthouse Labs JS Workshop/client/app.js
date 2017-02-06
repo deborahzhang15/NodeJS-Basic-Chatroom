522180348304bb0fe4be39e30ad4a3e27328ddc1
Deborah Zhang <deborahzhang15@gmail.com>
2017-02-06 02:22:53 -0800

    First commit to add the project to Github

---
+++ Lighthouse Labs JS Workshop/client/app.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+$('form').submit(function () {
+    var text = $('#initials').val() + " says: " + $('#message').val();
+
+    socket.emit('message', text);
+
+    $('#message').val('');
+    return false;
+});
+
+socket.on('message', function(msg){
+  $('<li>').text(msg).appendTo('#history');
+});
